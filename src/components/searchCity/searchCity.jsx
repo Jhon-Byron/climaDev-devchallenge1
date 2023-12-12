2f7a0a6c9b4e4569c3495848eddb08ebbc345d14
Jhon Byron <jhoncolombia66@gmail.com>
2023-12-11 22:46:45 -0300

    improved suggestions logic

--- src/components/searchCity/searchCity.jsx
+++ src/components/searchCity/searchCity.jsx
@@ -4,15 +4,26 @@ import InputWithSuggestions from '../form/inputWithSuggestions/inputWithSuggesti
 
 export default function SearchCity() {
   const [searchText, setSearchText] = useState('')
+  const [suggestions, setSuggestions] = useState([])
   
   const fetchCitiesList = async () => {
     const CITIES_LIMIT = 5
-    console.log(`http://api.openweathermap.org/geo/1.0/direct?q=${searchText}&limit=${CITIES_LIMIT}&appid=${process.env.NEXT_PUBLIC_OWM_KEY}`)
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchText}&limit=${CITIES_LIMIT}&appid=${process.env.NEXT_PUBLIC_OWM_KEY}`)
+    const json = await response.json()
+    handleList(json)
+  }
+
+  const handleList = (list) => {
+    const formattedList = list.map((obj) => ({
+      text: `${obj.name}, ${obj.state}, ${obj.country}`,
+      onClick: () => console.log(`Lat: ${obj.lat} lon: ${obj.lon}`)
+    }))
+    setSuggestions(formattedList)
   }
 
   useEffect(() => {
-    if (searchText.length >= 3)
-    fetchCitiesList()
+    if (searchText.length >= 3) fetchCitiesList()
+    else setSuggestions([])
   }, [searchText])
 
   return (
@@ -20,7 +31,7 @@ export default function SearchCity() {
       placeholder="Digite a cidade" 
       value={searchText}
       onChange={(e) => setSearchText(e.target.value)}
-      
+      suggestions={suggestions}
       />
   )
 }
