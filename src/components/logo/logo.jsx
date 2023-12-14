fcd4f0257d1d8b521f21ab6741c0228989dd704d
Jhon Byron <jhoncolombia66@gmail.com>
2023-12-13 22:46:25 -0300

    created nav bar

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,20 +1,23 @@
 import styled from "styled-components";
 
-const Img = styled.img`
-  
-`
+const Img = styled.img``;
 
 const VerticalText = styled.h1`
   font-size: 80px;
-  color: ${props => props.theme.colors.black};
+  color: ${(props) => props.theme.colors.black};
   line-height: 70px;
   margin-left: 150px;
   margin-top: -80px;
-`
+`;
+
+const HorizontalText = styled.h1`
+  font-size: 40px;
+`;
 
 const PrimaryColor = styled.span`
- color: ${props => props.theme.colors.primary};
-`
+  color: ${(props) => props.theme.colors.primary};
+  text-transform: uppercase;
+`;
 
 export default function Logo({ showImage, type }) {
   return (
@@ -22,8 +25,18 @@ export default function Logo({ showImage, type }) {
       {showImage && <img src="/woman.svg" />}
       {type === "vertical" && 
         <VerticalText>
-          Clima <br /><PrimaryColor>Dev</PrimaryColor>
-        </VerticalText>}
+          Clima <br />
+          <PrimaryColor>Dev</PrimaryColor>
+        </VerticalText>
+      }
+      {
+        type === "horizontal" && 
+        <HorizontalText>Clima<PrimaryColor>Dev</PrimaryColor></HorizontalText>
+      }
     </div>
   );
+}
+
+Logo.defaultProps = {
+  type: "horizontal"
 }
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,7 @@
+import Logo from "../logo/logo"
+
+export default function Navbar() {
+  return (
+    <Logo/>
+  )
+}
